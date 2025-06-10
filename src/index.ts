df4342ae66f6d324eae9751d552e86766da6b5c7
Jagadish Chauhan <9chauhanjagadish@gmail.com>
2025-06-10 11:28:56 +0530

    location-timezone-node

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as countries from './countries';
+import * as locations from './locations';
+import * as statesAnsi from './states-ansi';
+import * as timezones from './timezones';
+
+export = {
+  ...countries,
+  ...locations,
+  ...statesAnsi,
+  ...timezones,
+};
